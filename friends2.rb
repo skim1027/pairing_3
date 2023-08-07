db4b77d949c6289010e2246588abe2f8847daf49
Sooyung Kim <kimx1279@gmail.com>
2023-08-07 09:56:28 -0600

    Add integer iteration under 60

--- friends2.rb
+++ friends2.rb
@@ -1,5 +1,9 @@
 integers = [10, 20, 30, 40, 50, 60, 70, 80]
 
-integers.each do |integer|
-    p integer if integer > 30
+# integers.each do |integer|
+#     p integer if integer > 30
+# end
+
+integers.each do |under|
+    p under if under < 60
 end
